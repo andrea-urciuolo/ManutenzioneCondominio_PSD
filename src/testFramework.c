60a5b264a9996a51b22334e48f193e39428befa7
samuelesorbo <samuele.sorbo@gmail.com>
2026-05-05 10:25:30 +0200

    Add buildTechnician

--- src/testFramework.c
+++ src/testFramework.c
@@ -3,70 +3,79 @@
 #include <string.h>
 #include "testFramework.h"
 
-typedef enum {
-    INT_TEST,
-    CHAR_TEST
-} testType;
 
-struct testCase {
-    char name[50];
-    testType type;
-    int expectedInt;
-    char expectedChar;
-};
-
-testCase* createRequestTestSuite(int* size){
-    *size = 2;
-    testCase* suite = malloc(sizeof(testCase) * (*size));
-
-    strcpy(suite[0].name, "Request ID test");
-    suite[0].type = INT_TEST;
-    suite[0].expectedInt = 1;
-
-    strcpy(suite[1].name, "Request TYPE test");
-    suite[1].type = CHAR_TEST;
-    suite[1].expectedChar = 'a';
-
-    return suite;
-}
-
-testCase* createtechnicianTestSuite(int* size) {
-    *size = 2;
-    testCase* suite = malloc(sizeof(testCase) * (*size));
-
-    strcpy(suite[0].name, "Availability test");
-    suite[0].type = INT_TEST;
-    suite[0].expectedInt = 1;
-
-    strcpy(suite[1].name, "Interventions Test");
-    suite[1].type = INT_TEST;
-    suite[1].expectedInt = 1;
-
-    return suite;
-}
-
-testCase getTest(testCase* suite, int index){
-    return suite[index];
-}
-
-void compareInt (testCase t, int actual){
-    if (t.expectedInt == actual){
-        printf("PASS %s\n", t.name);
-    }
-    else {
-        printf("FAIL %s -> Expected: %d, got: %d\n", t.name, t.expectedInt, actual);
-    }
-}
-
-void compareChar(testCase t, char actual){
-    if(t.expectedChar == actual){
-        printf("PASS %s\n", t.name);
+void compareInt(char* name, int expected, int actual){
+    if (expected == actual){
+        printf("Pass: %s\n", name);
     }
     else{
-        printf("FAIL %s -> Expected: %c, got: %c\n", t.name, t.expectedChar, actual);
+        printf("Fail: %s\n", name);
     }
 }
 
-void freeTestSuite (testCase* suite){
-    free(suite);
+void compareChar(char* name, char expected, char actual){
+    if (expected == actual){
+        printf("Pass: %s\n", name);
+    }
+    else{
+        printf ("Fail: %s\n", name);
+    }
+}
+
+void runRequestTestSuite (char* filename){
+    FILE* f = fopen(filename, "r");
+
+    if (f == NULL){
+        printf("Error opening file\n");
+        return;
+    }
+
+    char name[50];
+    char type[10];
+
+    while (fscanf(f, "%s %s", name, type) != EOF){
+        if(strcmp(type, "INT") == 0){
+            int expected;
+            fscanf(f, "%d", &expected);
+
+            int actual = 1;
+
+            compareInt(name, expected, actual);
+        }
+
+        else if (strcmp(type, "CHAR") == 0){
+            char expected;
+            fscanf(f, " %c", &expected);
+
+            char actual = 'a';
+
+            compareChar(name, expected, actual);
+        }
+    }
+
+    fclose(f);
+}
+
+void runTechnicianTestSuite (char* filename){
+    FILE* f = fopen(filename, "r");
+
+    if (f == NULL){
+        printf("Error opening file\n");
+        return;
+    }
+
+    char name[50];
+    char type[10];
+
+    while (fscanf(f, "%s %s", name, type) != EOF){
+        if (strcmp(type, "INT") == 0){
+            int expected; 
+            fscanf(f, "%d", &expected);
+
+            int actual = 1;
+            compareInt(name, expected, actual);
+        }
+    }
+
+    fclose(f);
 }
