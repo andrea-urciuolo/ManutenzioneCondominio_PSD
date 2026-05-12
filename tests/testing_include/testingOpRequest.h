bd6468607f4de070bb7ba088b9b50d3b475e7935
verosimileale-ai <verosimileale@gmail.com>
2026-05-12 22:03:17 +0200

    Add testing function in testingOpRequest

--- tests/testing_include/testingOpRequest.h
+++ tests/testing_include/testingOpRequest.h
@@ -4,4 +4,7 @@
 
 int run_test_suite_printAllRequests(const char* input_path, const char* oracle_path);
 
+int run_test_suite_printRequestById(const char* input_path, const char* oracle_path);
+
+int run_test_suite_printRequestsByType(const char* input_path, const char* oracle_path);
 #endif /* TEST_SUITE_PRINTALLREQUESTS_H */
