2c8afcafe6ab558530ba5838faf79fc5cefa349b
samuelesorbo <samuele.sorbo@gmail.com>
2026-05-16 15:18:29 +0200

    Add test suite and oracle for Pqueue testing

--- tests/testing_src/pqueueTesting.c
+++ tests/testing_src/pqueueTesting.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../../include/PQueue.h"
 #include "../../include/request.h"
+#include "../testing_include/pqueueTesting.h"
 
 int run_test_suite_newPQ(const char* input_path, const char* oracle_path) {
     FILE *f_in, *f_orc;
